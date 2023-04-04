572c8fd72b3ead1d10e38b053080d778cc2be780
FrancisGG <frai.gonzalez@duocuc.cl>
2023-04-04 10:24:09 -0400

    contacto v1.0

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,3 +1,5 @@
+ /// <reference types="google.maps" />
+
 import { Component } from '@angular/core';
 
 
@@ -9,5 +11,30 @@ import { Component } from '@angular/core';
 export class ContactoComponent {
 
 }
+/*
 
+let map;
+async function initMap(): Promise<void> {
+  const position = { lat: -33.40838567608921, lng: -70.56459463109759};
+  const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+  const { Marker } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
 
+  map = new Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 10,
+      center: position,
+      mapId: 'a5b0b2c5c0b2b2c'
+    }
+  );
+
+  const marker = new Marker({
+    map: map,
+    position: position,
+    title: "Apoquindo 6415"
+  });
+}
+
+initMap();
+
+*/
