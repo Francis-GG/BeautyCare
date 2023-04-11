19bbb595c9cfd3c9687a6ee2bd5b98e30ab6d257
Bsol18 <Ba.ortiz@duocuc.cl>
2023-04-11 12:13:53 -0400

    navbar hecho completo responsive

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import {addDoc, Firestore, collection, getDocs} from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 
@@ -7,12 +7,29 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
   public data: any = [];
+  private subMenu!: HTMLElement;
+
   constructor(public firestore: Firestore, private router: Router) {
-    this.getData()
+    this.getData();
+    this.subMenu = document.getElementById("subMenu")!
   }
-  
+
+  ngAfterViewInit() {
+    this.subMenu = document.getElementById("subMenu")!;
+    if (!this.subMenu) {
+      console.error("Could not find subMenu element");
+    }
+  }
+
+  toggleMenu() {
+    const subMenu = document.getElementById("subMenu");
+    if (subMenu) {
+      subMenu.classList.toggle("open-menu");
+    }
+  }
+
   onClick(url: string){
     this.router.navigate([url]);
   }
@@ -25,8 +42,9 @@ export class NavbarComponent {
     })
     .catch((error) => {
       alert(error.message)
-    }
-  )}
+    });
+  }
+
   getData(){
     const dbInstance = collection(this.firestore, 'categorias');
     getDocs(dbInstance)
@@ -34,6 +52,6 @@ export class NavbarComponent {
       this.data = [...response.docs.map((item) => {
         return {...item.data(), id: item.id}
       })]
-    })
+    });
   }  
 }
