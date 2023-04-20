937637e384ff2322700808414a49c0c63ab75047
Bsol18 <ba.ortiz@duocuc.cl>
2023-04-20 10:42:16 -0400

    perfil actualizado falta

--- src/app/pages/perfil-cliente/perfil-cliente.component.ts
+++ src/app/pages/perfil-cliente/perfil-cliente.component.ts
@@ -8,3 +8,34 @@ import { Component } from '@angular/core';
 export class PerfilClienteComponent {
 
 }
+
+
+/*// Variables globales para la imagen y el botón
+const avatarImg = document.querySelector('#avatarImg');
+const avatarBtn = document.querySelector('#avatarBtn');
+
+// Controlador de eventos para cargar la imagen del botón
+avatarBtn.addEventListener('change', (e) => {
+  const file = e.target.files[0];
+  const storageRef = firebase.storage().ref(`avatars/${firebase.auth().currentUser.uid}/${file.name}`);
+  const uploadTask = storageRef.put(file);
+
+  // Controlador de eventos para la carga de la imagen
+  uploadTask.on('state_changed', (snapshot) => {
+    // Actualizar el botón de carga con el progreso de la carga
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log(`Carga de avatar en progreso: ${progress}%`);
+  }, (error) => {
+    // Mostrar mensaje de error si falla la carga de la imagen
+    console.error(`Error en la carga de avatar: ${error.message}`);
+  }, () => {
+    // Actualizar la imagen del avatar en la base de datos
+    uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+      avatarImg.src = downloadURL;
+      firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).update({
+        avatarURL: downloadURL
+      });
+    });
+  });
+});
+*/
