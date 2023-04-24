66d72681f82fd81db4b4d40b027b7492161bbfe1
FrancisGG <frai.gonzalez@duocuc.cl>
2023-04-24 04:33:09 -0400

    Servicios conectados a base de datos

--- src/app/pages/servicios/manicure/manicure.component.ts
+++ src/app/pages/servicios/manicure/manicure.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Firestore, collection, getDocs} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-manicure',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./manicure.component.css']
 })
 export class ManicureComponent {
-
+  public data: any = [];
+  constructor(public firestore: Firestore) {
+    this.getData()
+  }
+  getData(){
+    const dbInstance = collection(this.firestore, 'categorias/3/servicios');
+    getDocs(dbInstance)
+    .then((response) => {
+      this.data = [...response.docs.map((item) => {
+        return {...item.data(), id: item.id}
+      })]
+    })
+  }
 }
--- src/app/pages/servicios/peluqueria/peluqueria.component.ts
+++ src/app/pages/servicios/peluqueria/peluqueria.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Firestore, collection, getDocs} from '@angular/fire/firestore';
+
 
 @Component({
   selector: 'app-peluqueria',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./peluqueria.component.css']
 })
 export class PeluqueriaComponent {
-
+  public data: any = [];
+  constructor(public firestore: Firestore) {
+    this.getData()
+  }
+  getData(){
+    const dbInstance = collection(this.firestore, 'categorias/2/servicios');
+    getDocs(dbInstance)
+    .then((response) => {
+      this.data = [...response.docs.map((item) => {
+        return {...item.data(), id: item.id}
+      })]
+    })
+  }
 }
