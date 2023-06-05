688f86d8c98d9015128681a4b3de7650ff66a7bc
Bsol18 <Ba.ortiz@duocuc.cl>
2023-06-05 00:50:33 -0400

    servicios listos mas ts de manicure

--- src/app/pages/admin/manicure-admin/manicure-admin.component.ts
+++ src/app/pages/admin/manicure-admin/manicure-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Firestore, collection, getDocs, addDoc, doc, deleteDoc } from '@angular/fire/firestore';
+import { setDoc } from 'firebase/firestore';
 
 
 @Component({
@@ -11,6 +12,11 @@ export class ManicureAdminComponent {
   
   public data: any = [];
   public nextId: number = 0;
+  public idServicio: string = '';
+  public nombreServicio: string = '';
+  public descripcionServicio: string = '';
+  public precioServicio: string = '';
+  public duracionServicio: string = '';
 
   constructor(public firestore: Firestore) {
     this.getNextId();
@@ -35,6 +41,46 @@ export class ManicureAdminComponent {
       })]
     })
   }
+
+  obtenerIdServicio(idServicio: string, nombreServicio: string, descripcionServicio: string, duracionServicio: string, precioServicio: string) {
+    this.idServicio = idServicio;
+    this.nombreServicio = nombreServicio;
+    this.descripcionServicio = descripcionServicio;
+    this.precioServicio = precioServicio;
+    this.duracionServicio = duracionServicio;
+  }
+
+  async editarServicio(servicio: string, descripcion: string, precio: string, tiempo: string) {
+    try{
+      const serviceDocRef = doc(this.firestore, `categorias/3/servicios/${this.idServicio}`);
+      setDoc(serviceDocRef, {servicio, descripcion, precio, tiempo});
+      return true;
+    } catch (error) {
+      console.log('Error al intentar editar el servicio:', error);
+      return false;
+    }   
+  }
+
+  async handleEditService(formValue: any) {    
+    const nombre = formValue['nombre-edit-servicio'];
+    const descripcion = formValue['descripcion-edit-servicio'];
+    const precio = formValue['precio-edit-servicio'];
+    const duracion = formValue['duracion-edit-servicio'];
+    try{
+      await this.editarServicio(nombre, descripcion, precio, duracion);
+      alert('Servicio actualizado correctamente')
+      this.getData();
+    }catch(error){
+      console.log('Error al intentar actualizar el servicio:', error);
+      alert('Error al intentar actualizar el servicio');
+    }
+  }
+
+
+
+
+
+
 
   eliminarServicio(serviceId: string) {
     if (confirm('¿Está seguro que desea eliminar este servicio?')){
